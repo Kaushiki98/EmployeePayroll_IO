b73ef1d5fddb8584edb99c95b2e7dea16a9c7522
kaushiki <kaushiki.ak88@gmail.com>
2021-02-27 18:06:29 +0530

    ADDED: read and write employee payroll to console

---
+++ src/main/java/EmployeePayrollData.java
@@ -0,0 +1,11 @@
+public class EmployeePayrollData {
+    private int id;
+    private String name;
+    private double salary ;
+
+    public EmployeePayrollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
